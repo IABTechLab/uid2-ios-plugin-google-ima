aa21768e6f7a2045d83567d41a8ba4e1036fc228
Brad Leege <brad.leege@thetradedesk.com>
2023-04-03 13:11:02 -0500

    DevApp

---
+++ Development/UID2IMAPluginDevelopmentApp/UID2IMAPluginDevelopmentApp/UID2IMAPluginDevelopmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UID2IMAPluginDevelopmentAppApp.swift
+//  UID2IMAPluginDevelopmentApp
+//
+//  Created by Brad Leege on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct UID2IMAPluginDevelopmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
