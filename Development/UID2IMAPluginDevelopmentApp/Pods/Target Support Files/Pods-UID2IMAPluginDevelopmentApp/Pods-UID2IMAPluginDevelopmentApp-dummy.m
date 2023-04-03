6a5f1b8918c26d1b2c41d40c11089009e2273391
Brad Leege <brad.leege@thetradedesk.com>
2023-04-03 13:58:59 -0500

    Creating SDK Podspec and DevApp Podfile

---
+++ Development/UID2IMAPluginDevelopmentApp/Pods/Target Support Files/Pods-UID2IMAPluginDevelopmentApp/Pods-UID2IMAPluginDevelopmentApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UID2IMAPluginDevelopmentApp : NSObject
+@end
+@implementation PodsDummy_Pods_UID2IMAPluginDevelopmentApp
+@end
---
+++ Development/UID2IMAPluginDevelopmentApp/Pods/Target Support Files/UID2-IMA-Plugin/UID2-IMA-Plugin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UID2_IMA_Plugin : NSObject
+@end
+@implementation PodsDummy_UID2_IMA_Plugin
+@end
