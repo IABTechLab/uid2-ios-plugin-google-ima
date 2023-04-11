53dca85caad6370a88322393805628e6bc1e4a00
Brad Leege <brad.leege@thetradedesk.com>
2023-04-11 13:27:33 -0500

    Create DevApp

---
+++ Development/UID2GoogleIMADevelopmentApp/UID2GoogleIMADevelopmentApp/UID2GoogleIMADevelopmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UID2GoogleIMADevelopmentAppApp.swift
+//  UID2GoogleIMADevelopmentApp
+//
+//  Created by Brad Leege on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UID2GoogleIMADevelopmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
