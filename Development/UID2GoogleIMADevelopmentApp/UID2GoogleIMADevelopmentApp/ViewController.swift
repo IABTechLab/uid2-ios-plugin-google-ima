f763824b122c831d424f5cceca8acae185d8100e
Brad Leege <brad.leege@thetradedesk.com>
2023-04-12 11:49:57 -0500

    Converting DevApp To UIKit To Match IMA

---
+++ Development/UID2GoogleIMADevelopmentApp/UID2GoogleIMADevelopmentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UID2GoogleIMADevelopmentApp
+//
+//  Created by Brad Leege on 4/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
