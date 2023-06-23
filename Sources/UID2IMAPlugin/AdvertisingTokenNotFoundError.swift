a2c72970014249ca40372a69879821f3c865288e
Brad Leege <brad.leege@thetradedesk.com>
2023-06-23 09:10:50 -0500

    Making IMA Plugin Available To Objective-C (#7)

---
+++ Sources/UID2IMAPlugin/AdvertisingTokenNotFoundError.swift
@@ -0,0 +1,18 @@
+//
+//  AdvertisingTokenNotFoundError.swift
+//  
+//
+//  Created by Brad Leege on 3/23/23.
+//
+
+import Foundation
+
+/// Advertising Token Not Found for IMA Adapter
+@objc(AdvertisingTokenNotFoundError)
+public class AdvertisingTokenNotFoundError: NSError {
+    
+    convenience init() {
+        self.init(domain: "UID", code: 1)
+    }
+    
+}
